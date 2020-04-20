51b9206ec1fdf9b76beaf8a7cfcade8c8a51e454
Musla Uddin Opu <muslauddinopu@Muslas-MacBook-Pro.local>
2020-04-20 10:53:10 +0200

    created project with create react app and added files for homepage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { HomePage } from './homepage.compnent';
+import { HomePage } from './pages/homepage/homepage.component';
 
 function App() {
   return (
