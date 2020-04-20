d68c6d5d91696c5e71f3b682bd7abf5d39616c08
MuslaOpu <musla.opu@gmail.com>
2020-04-20 15:09:12 +0200

    update our menu-items to navigate withRouter HOC

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 
 import Directory from '../../components/directory/directory.component';
 
-import './homepage.style.scss';
+import './homepage.styles.scss';
 
-export const HomePage = () => (
-    <div className = 'homepage'>
-        <Directory/>
-    </div>
-)
+const HomePage = () => (
+  <div className='homepage'>
+    <Directory />
+  </div>
+);
 
+export default HomePage;
